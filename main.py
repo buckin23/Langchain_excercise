1bf4ef8e3c409838e150384b7557b2928bdb3f36
buckin23 <158106186+buckin23@users.noreply.github.com>
2024-02-08 01:36:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI, Query, File, UploadFile
-
 app = FastAPI()
 
-
-
-
-
 # User Interface queries
+
 @app.post("/answer_query_from_user_provided_text_file")
 async def answer_query_from_user_provided_text_file (query: str = Query(..., title="User Query", max_length=50), file: UploadFile = File(...)):
         if not query:
