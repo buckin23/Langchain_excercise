0321c627ac40ee624f497fdf848f641f668232e5
buckin23 <158106186+buckin23@users.noreply.github.com>
2024-02-08 01:35:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,11 @@ app = FastAPI()
 
 # User Interface queries
 @app.post("/answer_query_from_user_provided_text_file")
-async def answer_query_from_user_provided_text_file(query: str = Query(..., title="User Query", max_length=50), file: UploadFile = File(...)):
-
-if not query:
-        return {"answer": "Please provide a query"}
-    elif not file or not file.filename.endswith('.txt'):
-        return {"answer": "Please upload a .txt file"}
+async def answer_query_from_user_provided_text_file (query: str = Query(..., title="User Query", max_length=50), file: UploadFile = File(...)):
+        if not query:
+                return {"answer": "Please provide a query"}
+            elif not file or not file.filename.endswith('.txt'):
+                return {"answer": "Please upload a .txt file"}
       else:
         file_contents = await file.read()
         try:
